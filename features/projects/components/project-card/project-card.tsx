878ab9159070d5e3800e42de572d703164a56c21
arochat9 <32208646+arochat9@users.noreply.github.com>
2024-08-20 22:59:38 -0400

    Update project status enum values (#6)

--- features/projects/components/project-card/project-card.tsx
+++ features/projects/components/project-card/project-card.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import capitalize from "lodash/capitalize";
 import { Badge, BadgeColor } from "@features/ui";
 import { Routes } from "@config/routes";
 import { ProjectLanguage, ProjectStatus } from "@api/projects.types";
@@ -17,14 +16,15 @@ const languageNames = {
 };
 
 const statusColors = {
-  [ProjectStatus.stable]: BadgeColor.success,
+  [ProjectStatus.info]: BadgeColor.success,
   [ProjectStatus.warning]: BadgeColor.warning,
-  [ProjectStatus.critical]: BadgeColor.error,
+  [ProjectStatus.error]: BadgeColor.error,
 };
 
 export function ProjectCard({ project }: ProjectCardProps) {
   const { name, language, numIssues, numEvents24h, status } = project;
 
+  console.log("status", status);
   return (
     <div className={styles.container}>
       <div className={styles.topContainer}>
@@ -50,7 +50,7 @@ export function ProjectCard({ project }: ProjectCardProps) {
             <div className={styles.issuesNumber}>{numEvents24h}</div>
           </div>
           <div className={styles.status}>
-            <Badge color={statusColors[status]}>{capitalize(status)}</Badge>
+            <Badge color={statusColors[status]}>{statusMap(status)}</Badge>
           </div>
         </div>
       </div>
@@ -61,4 +61,17 @@ export function ProjectCard({ project }: ProjectCardProps) {
       </div>
     </div>
   );
+}
+
+function statusMap(status: string): string {
+  if (status === ProjectStatus.info) {
+    return "Stable";
+  }
+  if (status === ProjectStatus.warning) {
+    return "Warning";
+  }
+  if (status === ProjectStatus.error) {
+    return "Critical";
+  }
+  return "Unknown";
 }
